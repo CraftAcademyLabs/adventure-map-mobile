2f0484b3cb0340b98c88c0b854b1c3cadbda7842
Raoul DIFFOUO <diraulo@gmail.com>
2017-01-30 17:07:15 +0200

    install angular-mocks with bower instead of npm

--- tests/karma.conf.js
+++ tests/karma.conf.js
@@ -9,6 +9,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       '../www/lib/ionic/js/ionic.bundle.js',
+      '../www/lib/angular-mocks/angular-mocks.js',
       '../www/js/**/*.js',
       'specs/**/*.js'
     ],
