1c93b5a5cf75bb7174e8857b269e9dcfcd4ba21a
Raoul DIFFOUO <diraulo@gmail.com>
2017-01-30 18:22:52 +0200

    setup test coverage reporter

--- tests/karma.conf.js
+++ tests/karma.conf.js
@@ -21,12 +21,13 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      '../www/js/**/*.js': ['coverage']
     },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['mocha', 'coverage', 'progress'],
 
     // web server port
     port: 9876,
@@ -45,8 +46,10 @@ module.exports = function(config) {
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['PhantomJS'],
     plugins: [
-      'karma-phantomjs-launcher',
-      'karma-jasmine'
+      'karma-coverage',
+      'karma-mocha-reporter',
+      'karma-jasmine',
+      'karma-phantomjs-launcher'
     ],
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
@@ -54,6 +57,11 @@ module.exports = function(config) {
 
     // Concurrency level
     // how many browser should be started simultaneous
-    concurrency: Infinity
+    concurrency: Infinity,
+
+    coverageReporter: {
+      type : 'html',
+      dir : '../coverage/'
+    }
   })
 }
