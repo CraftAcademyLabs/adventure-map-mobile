f72fd3914b7ef3f5b570b85e837697a24e6f60f2
Raoul DIFFOUO <diraulo@gmail.com>
2017-01-31 11:48:30 +0200

    add npm test command and cleanup karma config file

--- tests/karma.conf.js
+++ tests/karma.conf.js
@@ -5,45 +5,21 @@ module.exports = function(config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine'],
-
-    // list of files / patterns to load in the browser
     files: [
       '../www/lib/ionic/js/ionic.bundle.js',
       '../www/lib/angular-mocks/angular-mocks.js',
       '../www/js/**/*.js',
       'specs/**/*.spec.js'
     ],
-
-    // list of files to exclude
-    exclude: [
-    ],
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    exclude: [],
     preprocessors: {
       '../www/js/**/*.js': ['coverage']
     },
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['mocha', 'coverage', 'progress'],
-
-    // web server port
     port: 9876,
-
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
-
-    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['PhantomJS'],
     plugins: [
       'karma-coverage',
@@ -51,14 +27,8 @@ module.exports = function(config) {
       'karma-jasmine',
       'karma-phantomjs-launcher'
     ],
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
     singleRun: false,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
     concurrency: Infinity,
-
     coverageReporter: {
       type : 'html',
       dir : '../coverage/'
