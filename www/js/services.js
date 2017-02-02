c59be20511438b4480e22c173eccdb4fd2d6b6f2
Amber <amber@amberwilkie.com>
2017-02-02 16:28:11 +0100

    Configure service to post to api/v1/activity

--- www/js/services.js
+++ www/js/services.js
@@ -1,8 +1,8 @@
 angular.module('adventureMap.services', [])
 
   .factory('createActivityService', function ($resource, API_URL) {
-    return $resource(API_URL + '/activity', {
-        'save': {
+    return $resource(API_URL + '/activity', {}, {
+        'post': {
           method: 'POST'
         }
       }
