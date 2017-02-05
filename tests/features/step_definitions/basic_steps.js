32b5ceca85df3bbe1a5ff8dbc8ba78e3fdbfa5da
tochman <thomas@craftacademy.se>
2017-02-05 13:14:54 +0100

    changes step def

--- tests/features/step_definitions/basic_steps.js
+++ tests/features/step_definitions/basic_steps.js
@@ -12,21 +12,21 @@ var basicStepDefinitionsWrapper = function () {
   });
 
   this.Given(/^I go to "([^"]*)"$/, function (site, callback) {
-    browser.get(site).then(function(){
+    browser.get(site).then(function () {
       callback();
     });
 
   });
 
   this.Given(/^I open the app$/, function (callback) {
-    browser.get('').then(function(){
-      callback();
-    });
+    browser.get('');
+    callback();
+
   });
 
   this.Given(/^I fill in "([^"]*)" with "([^"]*)"$/, function (element, value, callback) {
     var field = browser.element(by.css('body')).element(by.css('input[placeholder="' + element + '"]'));
-    field.sendKeys(value).then(function(){
+    field.sendKeys(value).then(function () {
       callback();
     });
   });
@@ -34,20 +34,20 @@ var basicStepDefinitionsWrapper = function () {
 
   this.Then(/^I click "([^"]*)"$/, function (element, callback) {
     var button = browser.element(by.buttonText(element));
-    button.click().then(function(){
+    button.click().then(function () {
       callback();
     });
 
   });
 
   this.Given(/^I am logged in as "([^"]*)" with password "([^"]*)"$/, function (username, password, callback) {
-    browser.get('').then(function(){
+    browser.get('').then(function () {
       var emailField = browser.element(by.css('body')).element(by.css('input[placeholder="Email"]'));
       var passwordField = browser.element(by.css('body')).element(by.css('input[placeholder="Password"]'));
       var loginButton = browser.element(by.buttonText("Login"));
       emailField.sendKeys(username);
       passwordField.sendKeys(password);
-      loginButton.click().then(function(){
+      loginButton.click().then(function () {
         callback();
       });
     });
