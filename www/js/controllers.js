879092bad76cb73fe31183c260b1bc544efe650b
Amber <amber@amberwilkie.com>
2017-02-09 11:20:23 +0100

    Add filters to menu for activities page

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,6 +6,7 @@ angular
   .controller('userController', userController);
 
 function activitiesController($scope, $state, $ionicLoading, $ionicModal, $ionicSideMenuDelegate, Activity) {
+  $scope.message = 'info from activitiesController';
   $scope.filters = {};
   $scope.showFilters = false;
   $scope.$on("$ionicView.enter", function () {
@@ -13,6 +14,7 @@ function activitiesController($scope, $state, $ionicLoading, $ionicModal, $ionic
       template: 'Getting activities...'
     });
     Activity.query(function (response) {
+      console.log('getting activities');
       $scope.activities = response.activities.reverse();
       $scope.cachedActivities = $scope.activities; // This keeps the entire activity list so users can un-filter.
       $ionicLoading.hide();
@@ -34,50 +36,9 @@ function activitiesController($scope, $state, $ionicLoading, $ionicModal, $ionic
     $state.go('create_activity');
   };
 
-  $scope.toggleFilters = function () {
-    $scope.showFilters = !$scope.showFilters;
-    console.dir(Object);
-    // debugger;
-    $ionicSideMenuDelegate.toggleLeft();
-    // $ionicModal.fromTemplateUrl('templates/filters.html', {
-    //   scope: $scope,
-    //   animation: 'slide-in-up'
-    // }).then(function(modal) {
-    //   $scope.modal = modal;
-    //   $scope.openModal();
-    // });
-
-    $scope.openModal = function () {
-      $scope.modal.show();
-    }
-  };
-
   $scope.setFilters = function () {
     console.log($scope.filters);
-    $ionicSideMenuDelegate.toggleLeft();
-    $scope.showFilters = !$scope.showFilters;
-    $scope.activities = $scope.activities.filter(function (activity) {
-      if ($scope.filters.difficulty1) {
-        if (activity.difficulty == 1) {
-          console.log(activity);
-          return activity;
-        }
-      }
-      if ($scope.filters.difficulty2) {
-        if (activity.difficulty == 2) {
-          console.log(activity);
-          return activity;
 
-        }
-      }
-      if ($scope.filters.difficulty3) {
-        if (activity.difficulty == 3) {
-          console.log(activity);
-          return activity;
-
-        }
-      }
-    })
   };
 
   $scope.addActivity = function () {
