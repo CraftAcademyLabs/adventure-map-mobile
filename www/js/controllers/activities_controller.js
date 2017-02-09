72c425e335831f181a9e6bc2daa13fd23359e64b
Amber <amber@amberwilkie.com>
2017-02-09 14:16:45 +0100

    Refactor activities controller to /controllers/activities

--- www/js/controllers/activities_controller.js
+++ www/js/controllers/activities_controller.js
@@ -1,5 +1,8 @@
 function activitiesController($scope, $state, $ionicLoading, Activity) {
   $scope.message = 'This is the Activities View for ' + $scope.user.email;
+  $scope.filters = {};
+  [$scope.star1, $scope.star2, $scope.star3, $scope.star4, $scope.star5] = [false, false, false, false, false];
+
 
   $scope.$on("$ionicView.enter", function () {
     $ionicLoading.show({
@@ -7,6 +10,7 @@ function activitiesController($scope, $state, $ionicLoading, Activity) {
     });
     Activity.query(function (response) {
       $scope.activities = response.activities.reverse();
+      $scope.cachedActivities = $scope.activities; // This keeps the entire activity list so users can un-filter.
       $ionicLoading.hide();
     });
   });
@@ -18,4 +22,45 @@ function activitiesController($scope, $state, $ionicLoading, Activity) {
   $scope.viewProfile = function () {
     $state.go('app.profile');
   }
+
+  $scope.setFilters = function () {
+    var rating = 1;
+    if ($scope.star5) {
+      rating = 5
+    } else if ($scope.star4) {
+      rating = 4
+    } else if ($scope.star3) {
+      rating = 3
+    } else if ($scope.star2) {
+      rating = 2
+    } else {
+      rating = 1
+    }
+    $scope.filters.rating = rating;
+    console.log($scope.filters);
+
+  };
+
+  $scope.toggleStars = function (star_id) {
+    switch (star_id) {
+      case 1:
+        [$scope.star1, $scope.star2, $scope.star3, $scope.star4, $scope.star5] = [true, false, false, false, false];
+        break;
+      case 2:
+        [$scope.star1, $scope.star2, $scope.star3, $scope.star4, $scope.star5] = [true, true, false, false, false];
+        break;
+      case 3:
+        [$scope.star1, $scope.star2, $scope.star3, $scope.star4, $scope.star5] = [true, true, true, false, false];
+        break;
+      case 4:
+        [$scope.star1, $scope.star2, $scope.star3, $scope.star4, $scope.star5] = [true, true, true, true, false];
+        break;
+      case 5:
+        [$scope.star1, $scope.star2, $scope.star3, $scope.star4, $scope.star5] = [true, true, true, true, true];
+        break;
+      default:
+        [$scope.star1, $scope.star2, $scope.star3, $scope.star4, $scope.star5] = [false, false, false, false, false];
+
+    }
+  };
 }
