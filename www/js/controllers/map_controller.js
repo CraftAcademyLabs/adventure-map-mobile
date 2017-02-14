9398508e775b99239eea0c4cfe82e098d4ba3bc7
Amber <amber@amberwilkie.com>
2017-02-14 15:00:25 +0100

    Display map on map page

--- www/js/controllers/map_controller.js
+++ www/js/controllers/map_controller.js
@@ -1,3 +1,13 @@
 function mapController() {
+  var mymap = L.map('mapid').setView([57.7, 11.97], 10);
+  console.dir(mymap);
 
+  var marker = L.marker([57.6, 11.97]).addTo(mymap);
+
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.outdoors',
+    accessToken: 'pk.eyJ1IjoiYXF1YWFtYmVyIiwiYSI6ImNpejVreGVxNzAwNTEyeXBnbWc5eXNlcTYifQ.ah37yE5P2LH9LVzNelgymQ'
+  }).addTo(mymap);
 }
