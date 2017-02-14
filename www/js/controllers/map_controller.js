c13632db1057494e58e61a569ae12977ceb82824
Amber <amber@amberwilkie.com>
2017-02-14 15:18:17 +0100

    Move controls to bottom left

--- www/js/controllers/map_controller.js
+++ www/js/controllers/map_controller.js
@@ -1,8 +1,14 @@
 function mapController() {
-  var mymap = L.map('mapid').setView([57.7, 11.97], 10);
-  console.dir(mymap);
+  var mymap = L.map('mapid', {
+    zoomControl: false
+  })
+    .setView([57.7, 11.97], 10);
 
-  var marker = L.marker([57.6, 11.97]).addTo(mymap);
+  L.control.zoom({
+    position: 'bottomleft'
+  }).addTo(mymap);
+
+  L.marker([57.6, 11.97]).addTo(mymap);
 
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
