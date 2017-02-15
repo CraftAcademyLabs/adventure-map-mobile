08ff3776f8612a48c26abc8c90a43e45228359e7
tochman <thomas@craftacademy.se>
2017-02-15 21:29:43 +0100

    refactors mapController

--- www/js/controllers/map_controller.js
+++ www/js/controllers/map_controller.js
@@ -1,39 +1,48 @@
+function mapController($scope, $cordovaGeolocation, $ionicLoading) {
 
-function mapController($cordovaGeolocation) {
+  var posOptions = {maximumAge: 3000, timeout: 5000, enableHighAccuracy: true};
 
-  let posOptions = {timeout: 10000, enableHighAccuracy: false};
-  $cordovaGeolocation
-    .getCurrentPosition(posOptions)
-    .then(function (position) {
-      let lat  = position.coords.latitude;
-      let long = position.coords.longitude;
-      console.log(lat + ', ' + long);
-      mymap.setView([lat, long], 10); // Center the map on user's geolocation after it loads.
-    }, function(err) {
-      // error
+  ionic.Platform.ready(function(){
+    $ionicLoading.show({
+      template: 'Loading current location...'
     });
+    $cordovaGeolocation
+      .getCurrentPosition(posOptions)
+      .then(function (position) {
+        var lat = position.coords.latitude;
+        var long = position.coords.longitude;
+        console.log(lat + ', ' + long);
+        addmap(lat, long);
+        $ionicLoading.hide();
+      }, function (err) {
+        // error
+      });
+  });
 
 
-  // Create a map, center it on Gothenburg.
-  const mymap = L.map('mapContainer', {
-    zoomControl: false
-  })
-    .setView([57.7, 11.97], 10);
 
-  // Add zoom controls
-  L.control.zoom({
-    position: 'bottomleft'
-  }).addTo(mymap);
+  function addmap(lat, long){
+    const myMap = L.map('mapContainer', {
+        zoomControl: false
+      })
+      .setView([lat, long], 13);
 
-  // A marker example
-  L.marker([57.6, 11.97]).addTo(mymap);
+    // Add zoom controls
+    L.control.zoom({
+      position: 'bottomleft'
+    }).addTo(myMap);
+
+    // A marker example
+    L.marker([lat, long]).addTo(myMap);
 
 
-  // The map "tile layer"
-  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.outdoors',
-    accessToken: 'pk.eyJ1IjoiYXF1YWFtYmVyIiwiYSI6ImNpejVreGVxNzAwNTEyeXBnbWc5eXNlcTYifQ.ah37yE5P2LH9LVzNelgymQ'
-  }).addTo(mymap);
+    // The map "tile layer"
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox.outdoors',
+      accessToken: 'pk.eyJ1IjoiYXF1YWFtYmVyIiwiYSI6ImNpejVreGVxNzAwNTEyeXBnbWc5eXNlcTYifQ.ah37yE5P2LH9LVzNelgymQ'
+    }).addTo(myMap);
+  }
+
 }
