c2a150edc731a75d29e6fbebea71e40dc9d46f72
tochman <thomas@craftacademy.se>
2017-02-17 14:37:59 +0100

    adds watching method draws line between ols position and new position on watch

--- www/js/controllers/map_controller.js
+++ www/js/controllers/map_controller.js
@@ -1,55 +1,74 @@
-function mapController($cordovaGeolocation, $cordovaBackgroundGeolocation, $ionicLoading) {
+function mapController($cordovaGeolocation, $ionicLoading) {
 
-  var posOptions = {maximumAge: 3000, timeout: 5000, enableHighAccuracy: true};
+  var posOptions = {
+    maximumAge: 3000,
+    timeout: 5000,
+    enableHighAccuracy: true
+  };
 
-  ionic.Platform.ready(function () {
-    $ionicLoading.show({
-      template: 'Loading current location...'
-    });
-    debugger;
-    $cordovaGeolocation
-      .getCurrentPosition(posOptions)
-      .then(function (position) {
-        var lat = position.coords.latitude;
-        var long = position.coords.longitude;
-        console.log(lat + ', ' + long);
-        addmap(lat, long);
-        $ionicLoading.hide();
-      }, function (err) {
-        // error
-      });
+  var watchOptions = {
+    maximumAge: 3000,
+    timeout: 5000,
+    enableHighAccuracy: true // may cause errors if true
+  };
 
-    var watchOptions = {
-      timeout: 3000,
-      enableHighAccuracy: false // may cause errors if true
-    };
+  var polOptions = {
+    color: 'blue',
+    weight: 3,
+    opacity: 0.5,
+    smoothFactor: 1
+  };
 
-    var watch = $cordovaGeolocation.watchPosition(watchOptions);
-    watch.then(
-      null,
-      function (err) {
-        // error
-      },
-      function (position) {
-        var lat = position.coords.latitude;
-        var long = position.coords.longitude;
-        console.log(lat + ', ' + long);
-        L.marker([lat, long]).addTo(myMap);
-      });
+  var lat, long;
+
+  const map = L.map('mapContainer', {
+    zoomControl: false
+  });
+  const geolocation = $cordovaGeolocation.getCurrentPosition(posOptions);
+  const watch = $cordovaGeolocation.watchPosition(watchOptions);
 
 
-    watch.clearWatch();
+  $ionicLoading.show({
+    template: 'Loading current location...'
   });
 
+  geolocation.then(function (position) {
+    lat = position.coords.latitude;
+    long = position.coords.longitude;
+    console.log(lat + ', ' + long);
 
-  function addmap(lat, long) {
-    const myMap = L.map('mapContainer', {
-        zoomControl: false
-      })
-      .setView([lat, long], 13);
+    map.setView([lat, long], 13);
+    addToMap(lat, long, map);
+    $ionicLoading.hide();
+
+
+  }, function (err) {
+    // error
+  });
+
+  watch.then(
+    null,
+    function (err) {
+      // error
+      console.log(err);
+    },
+    function (position) {
+      var old_lat = lat;
+      var old_long = long;
+      lat = position.coords.latitude;
+      long = position.coords.longitude;
+      console.log(lat + ', ' + long);
+      addMarker(lat, long, old_lat, old_long, map)
+    });
+
+
+  // watch.clearWatch();
+
+
+  function addToMap(lat, long, map) {
 
     // A marker example
-    L.marker([lat, long]).addTo(myMap);
+    L.marker([lat, long]).addTo(map);
 
     // The map "tile layer"
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
@@ -57,11 +76,23 @@ function mapController($cordovaGeolocation, $cordovaBackgroundGeolocation, $ioni
       maxZoom: 18,
       id: 'mapbox.outdoors',
       accessToken: 'pk.eyJ1IjoiYXF1YWFtYmVyIiwiYSI6ImNpejVreGVxNzAwNTEyeXBnbWc5eXNlcTYifQ.ah37yE5P2LH9LVzNelgymQ'
-    }).addTo(myMap);
+    }).addTo(map);
 
     L.control.scale({
       imperial: false
-    }).addTo(myMap);
+    }).addTo(map);
+
+  }
+
+
+  function addMarker(lat, long, old_lat, old_long, map) {
+    var pointA = new L.LatLng(old_lat, old_long);
+    var pointB = new L.LatLng(lat, long);
+    var pointList = [pointA, pointB];
+
+    var polyline = new L.polyline(pointList, polOptions);
+    polyline.addTo(map);
+    map.setView([lat, long], 13);
 
   }
 
