24c65cc7da6d8ab5dbb6d877df82ca200a0ef88b
Amber <amber@amberwilkie.com>
2017-02-21 17:08:13 +0100

    Use a modal for the comment

--- www/js/controllers/show_activity_controller.js
+++ www/js/controllers/show_activity_controller.js
@@ -1,4 +1,4 @@
-function showActivityController($scope, $ionicModal, $ionicLoading, Activity, Comment){
+function showActivityController($scope, $rootScope, $ionicModal, $ionicLoading, Activity, Comment){
   $scope.openModal = function (activity) {
     $ionicModal.fromTemplateUrl('templates/activity.html', {
       scope: $scope,
@@ -17,18 +17,32 @@ function showActivityController($scope, $ionicModal, $ionicLoading, Activity, Co
     $scope.modal.remove();
   };
 
-  $scope.makeComment = function(activityId) {
+  $scope.openCommentBox = function(activityId, userId) {
+    $ionicModal.fromTemplateUrl('templates/comment.html', {
+      scope: $scope,
+      animation: 'zoom-from-center'
+    }).then(function (modal) {
+      $scope.modal = modal;
+      console.log('what up');
+      $scope.modal.show();
+    });
+  };
+
+  $scope.makeComment = function(activityId, userId) {
     var commentData = {};
-    // commentData.id = $scope.user.id;
-    console.log(activityId);
+    commentData.id = $scope.user.id;
     commentData.activity_id = activityId;
+    var body = 'A fake body';
+    // commentData.body = 'A fake body';
     $ionicLoading.show({
       template: 'Saving comment...'
     });
-    Comment.save({id: activityId, commentData}, function (resp) {
+    console.log(userId);
+    Comment.save({body: body, id: activityId}, function (resp) {
       // $state.go('app.activities');
       $ionicLoading.hide();
       console.log(resp);
+      $scope.closeModal();
     }, function (resp) {
       $ionicLoading.hide();
       console.log(resp);
