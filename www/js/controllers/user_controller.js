ea09e7cc2587ff1e5839b4d72cf3c747934a6f21
Raoul DIFFOUO <diraulo@gmail.com>
2017-03-02 22:26:39 +0200

    move userController to its own file

---
+++ www/js/controllers/user_controller.js
@@ -0,0 +1,31 @@
+function userController($scope, $ionicPlatform) {
+  $scope.files = [];
+
+  $ionicPlatform.ready(function () {
+    try {
+      window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function (dirEntry) {
+        var directoryReader = dirEntry.createReader();
+        directoryReader.readEntries(dirSuccess, dirFail);
+      });
+    } catch(e) {
+      console.log("Corvova plugins aren't available in browsers.");
+      console.log(e);
+    }
+
+    function dirSuccess(entries) {
+      console.log("INFO: Listing entries");
+      var i;
+      for (i = 0; i < entries.length; i++) {
+        var timeStampFromFileName = new Date(parseFloat(entries[i].name.replace(/\.[^/.]+$/, "")));
+        $scope.files.push({
+          fileName: entries[i].name,
+          date: timeStampFromFileName
+        });
+      }
+    }
+
+    function dirFail(error) {
+      console.log("Failed to list directory contents: " + error.code);
+    }
+  });
+}
